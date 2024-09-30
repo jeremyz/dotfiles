c23aabf841d1414bb3ad75db51c0f1193cb0bb61
Jérémy Zurcher <jeremy@asynk.ch>
2024-09-30 14:57:05 +0200

    gitsigns config

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -24,13 +24,8 @@ require("lazy").setup {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+      numhl      = true,
+      word_diff = true,
     },
   },
   {
