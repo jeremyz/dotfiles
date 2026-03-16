12c5c1df9d847c6e9672c25a97781a47db67e49f
Jérémy Zurcher <jeremy@asynk.ch>
2026-03-16 11:28:10 +0100

    nvim.init : autoremove empty note file

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -16,3 +16,17 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "**/task/notes/*.md",
+  callback = function()
+    local file_path = vim.fn.expand("%")
+    if file_path == "" or vim.fn.filereadable(file_path) == 0 then
+      return
+    end
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    if #lines <= 1 and lines[1] == "" then
+      os.remove(file_path)
+    end
+  end,
+})
